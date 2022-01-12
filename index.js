03fcef0c2426ff1bbc914c172f78c86be722e4a1
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-12 09:18:03 -0800

    commit tree 267ff784015b17440375834a0d00f7827b1ea6d7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+//below is a variable that calls an anonymous function
+const returnFirstTwoDrivers = (arrayOfDrivers) => {
+    // let firstTwo = arrayOfDrivers.slice(0,2)
+    // return firstTwo
+    //or
+    return arrayOfDrivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = (arrayOfDrivers) => {
+    let lastTwo = arrayOfDrivers.slice(-2)
+    return lastTwo
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    //below is an an anonymous function with fare as a  that we are returning
+    return (fare) => {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+fareDoubler(10)
+
+//below is a function that returns a function and the inner function is immediately invoked with the parameters passed.
+//console.log(createFareMultiplier(2)(10)) // returns 20
+
+const fareTripler = createFareMultiplier(3)
+fareTripler(12)
+
+//below is a funciton named selectDifferentDrivers which takes an array and a function (here the function it wants to call are the returnfirst and returnlast functions above) 
+function selectDifferentDrivers(arrayOfDrivers, selectDriversFunction) {
+    //below is the return of the function passed as the second parameter above, with arrayOfDrivers passed as the parameter
+    return selectDriversFunction(arrayOfDrivers)
+}
